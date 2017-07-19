2c7ce743eab1b435646bc4ffe25c511516d1cc6a
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-07-19 03:53:17 +0200

    unarchive function

--- www/views/archive/archive.controller.js
+++ www/views/archive/archive.controller.js
@@ -6,9 +6,30 @@ function ArchiveController($scope, $state, $firebaseArray, $firebaseObject) {
 	var me = this;
 
 	me.goHome = goHome;
+	me.init = init;
+	me.markAsNonRead = markAsNonRead;
+
+	function init() {
+		var ref = firebase.database().ref().child("messages");
+
+		me.list = $firebaseObject(ref);
+
+		me.list.$bindTo($scope, "data").then(function() {
+			console.log($scope.data);
+		});
+	}
 
 	function goHome(){
 		$state.go('home');
 	}
+
+	function markAsNonRead(messageId) {
+		me.list[messageId].status = 'close';
+		me.list.$save().then(function(a){
+			console.log(a);
+		});
+	}
+
+	init();
 
 }
